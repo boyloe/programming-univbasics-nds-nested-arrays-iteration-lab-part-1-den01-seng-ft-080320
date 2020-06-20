3c17b9a76a121dcd929426e92e8df19ffc119573
Bryan Oyloe <boyloe@gmail.com>
2020-06-20 06:08:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,11 +8,12 @@ def find_even_values(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index] % 2 == 0 
         even_numbers << src[row_index][element_index]
-        binding.pry
+        
       end   
       element_index += 1 
     end  
     row_index += 1 
   end
   even_numbers
+  binding.pry
 end  
